362101859fc784edcf835b19ab279376149fa46c
James Hager <james@avantiapplications.com>
2022-01-25 09:42:14 -0700

    Added problem statement

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,7 +1,12 @@
+//
+//  Given a number, n, return an array containing n unique random numbers between 1-10, inclusive. (That is, do not repeat any numbers in the returned list.)
+//
+//  You can trust that this function will never be called with n < 0 or n > 10.
+//
+
 // Write your code below this line.
 
 function luckyNum(n){
-
     let luckyNum = []
     while (luckyNum.length < n) {
         let num = Math.floor(Math.random() * 10)
