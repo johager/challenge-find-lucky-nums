3203dc11368815cef5ebce64b82a5aba9a90ee38
James Hager <james@avantiapplications.com>
2022-01-25 09:37:16 -0700

    Completed

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,15 @@
 // Write your code below this line.
+
+function luckyNum(n){
+
+    let luckyNum = []
+    while (luckyNum.length < n) {
+        let num = Math.floor(Math.random() * 10)
+        if (!luckyNum.includes(num)) {
+            luckyNum.push(num)
+        }
+    }
+    return luckyNum
+}
+
+console.log(luckyNum(5))
